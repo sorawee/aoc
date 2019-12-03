f6df88203042df73e7d6506d0a08e1ca6f82f1c7
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2019-12-02 21:53:55 -0800

    +day 3

---
+++ 3/task.rkt
@@ -0,0 +1,57 @@
+#lang racket
+
+(require "../aoc.rkt")
+
+(define (draw locations xs name)
+  (for/fold ([x 0] [y 0] [t 0]) ([item xs])
+    (match-define (pregexp "(.)(.+)" (list _ dir (app string->number len))) item)
+    (define (update-x i)
+      (match dir
+        ["R" (+ i 1)]
+        ["L" (- (+ i 1))]
+        ["U" 0]
+        ["D" 0]))
+    (define (update-y i)
+      (match dir
+        ["R" 0]
+        ["L" 0]
+        ["U" (+ i 1)]
+        ["D" (- (+ i 1))]))
+    (for ([i len])
+      (hash-update! locations (list (+ x (update-x i)) (+ y (update-y i)))
+                    (curry cons (list name (+ t i 1))) '()))
+    (values (+ x (update-x (sub1 len)))
+            (+ y (update-y (sub1 len)))
+            (+ t len))))
+
+(define (calc-min locations proc)
+  (inexact->exact
+   (for*/fold ([min-val +inf.0])
+              ([(key val) (in-hash locations)]
+               [visited (in-value (remove-duplicates (map first val)))]
+               #:when (> (length visited) 1))
+     (min min-val (proc key val)))))
+
+(tests
+ (task-1 "R8,U5,L5,D3\nU7,R6,D4,L4") ==> 6
+ (task-1 "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83") ==> 159
+ (task-1 "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7") ==> 135)
+
+(define-task task-1
+  (define locations (make-hash))
+  (for ([line (in-lines)] [i (in-naturals)])
+    (draw locations (string-split line ",") i))
+  (calc-min locations (λ (key val)
+                        (match-define (list x y) key)
+                        (+ (abs x) (abs y)))))
+
+(tests
+ (task-2 "R8,U5,L5,D3\nU7,R6,D4,L4") ==> 30
+ (task-2 "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83") ==> 610
+ (task-2 "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7") ==> 410)
+
+(define-task task-2
+  (define locations (make-hash))
+  (for ([line (in-lines)] [i (in-naturals)])
+    (draw locations (string-split line ",") i))
+  (calc-min locations (λ (key val) (apply + (map second val)))))
